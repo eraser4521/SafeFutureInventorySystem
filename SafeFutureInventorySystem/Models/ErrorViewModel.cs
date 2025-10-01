348681beb9ac0179bed9e614ce0d0250e9df5e81
Alexis Campos <n01478711@unf.edu>
2025-09-30 23:40:47 -0400

    Final Base Files
    Initial Base Files for Senior Project Repository

---
+++ SafeFutureInventorySystem/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace SafeFutureInventorySystem.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
