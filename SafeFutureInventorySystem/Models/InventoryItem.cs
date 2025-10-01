558b65ce6b010f502ae7739500d15066f9ef324a
Alexis Campos <n01478711@unf.edu>
2025-09-30 23:49:24 -0400

    SortedInventory - Initial Attempt
    Working Attempt at showing sorted inventory system.

---
+++ SafeFutureInventorySystem/Models/InventoryItem.cs
@@ -0,0 +1,9 @@
+﻿namespace SafeFutureInventorySystem.Models;
+
+public class InventoryItem
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public int Quantity { get; set; }
+    public DateTime DateAdded { get; set; }
+}
